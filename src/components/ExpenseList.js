7650206b146fdba37304145786e98bdaa61b0c8d
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-26 21:09:18 +0200

    Handle empty expense list

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,27 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Card from './Card';
+import empty from '../assets/empty.svg';
 
 const ExpenseList = () => {
-  const list = [
-    {
-      title: 'Made a purchase',
-      logo: 'logo',
-      createdAt: Date.now(),
-      amount: '4567$',
-    },
-    {
-      title: 'Made a purchase',
-      logo: 'logo',
-      createdAt: Date.now(),
-      amount: '4567$',
-    },
-  ];
+  const { expenseList: list } = useSelector((state) => state.expenses);
 
   return (
     <div className='mt-6'>
-      {list?.map((item) => (
-        <Card item={item} />
-      ))}
+      {list.length ? (
+        list.map((item) => <Card item={item} />)
+      ) : (
+        <div className='mt-10'>
+          <img
+            src={empty}
+            alt='empty'
+            className='bg-white p-6 rounded-lg w-full max-w-md mx-auto'
+          />
+          <p className='bg-white w-max mx-auto p-2 rounded-lg mt-4 font-semibold'>
+            Oh, you don't spend money? 🤔
+          </p>
+        </div>
+      )}
     </div>
   );
 };
