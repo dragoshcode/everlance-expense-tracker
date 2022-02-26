3e584056a52f9747a7a874ed992d64ce8ac8e4a3
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-27 00:09:26 +0200

    Store expenses in local storage

--- src/redux/reducers/expenses.js
+++ src/redux/reducers/expenses.js
@@ -1,12 +1,27 @@
 import { ADD_EXPENSE, DELETE_EXPENSE } from '../action-types/expenses';
 
+const initalList = () => {
+  const list = localStorage.getItem('expense-list');
+
+  let expenses = [];
+
+  if (list) {
+    expenses = JSON.parse(list);
+  }
+  return expenses;
+};
+
 const initialState = {
-  expenseList: [],
+  expenseList: initalList(),
 };
 
 export const expenseReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_EXPENSE: {
+      localStorage.setItem(
+        'expense-list',
+        JSON.stringify([...state.expenseList, action.data])
+      );
       return {
         ...state,
         expenseList: [...state.expenseList, action.data],
@@ -18,6 +33,8 @@ export const expenseReducer = (state = initialState, action) => {
       const updatedList = state.expenseList.filter(
         (item) => item.createdAt !== data.createdAt
       );
+      
+      localStorage.setItem('expense-list', JSON.stringify(updatedList));
       return {
         ...state,
         expenseList: updatedList,
